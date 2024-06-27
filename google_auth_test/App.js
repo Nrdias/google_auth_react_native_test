ce180d01780def364a203b7f28a516fc1e3b9c7f
Dias <natan.dias@edu.pucrs.br>
2024-06-27 23:08:12 +0000

    app for test

---
+++ google_auth_test/App.js
@@ -0,0 +1,48 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+import { GoogleSignin, statusCodes, GoogleSigninButton } from '@react-native-google-signin/google-signin';
+
+export default function App() {
+
+  React.useEffect(() =&gt; {
+    GoogleSignin.configure({
+      webClientId: "824546136739-g11csddbqrh6tjh6m1in5kfima42ohtg.apps.googleusercontent.com", 
+      offlineAccess: true
+    });
+  }, [])
+
+  const GoogleSingUp = async () =&gt; {
+    try {
+      await GoogleSignin.hasPlayServices();
+      await GoogleSignin.signIn().then(result =&gt; { console.log(result) });
+    } catch (error) {
+      if (error.code === statusCodes.SIGN_IN_CANCELLED) {
+        alert('User cancelled the login flow !');
+      } else if (error.code === statusCodes.IN_PROGRESS) {
+        alert('Signin in progress');
+      } else if (error.code === statusCodes.PLAY_SERVICES_NOT_AVAILABLE) {
+        alert('Google play services not available or outdated !');
+      } else {
+        console.log(error)
+      }
+    }
+  };
+  return (
+    <View style={styles.container}>
+      <GoogleSigninButton
+        size={GoogleSigninButton.Size.Wide}
+        color={GoogleSigninButton.Color.Dark}
+        onPress={GoogleSignin}
+      />;
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
